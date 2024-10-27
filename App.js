ba12d0cf3b6e9beb5da23653a0c430224ad4fb8b
Osama-Arain <osaamaarain@gmail.com>
2024-10-27 14:07:44 +0500

    Color FLipper commit

---
+++ App.js
@@ -0,0 +1,13 @@
+let colorDisplay = document.getElementById('colorDisplay');
+
+
+ 
+function colorflipper(){
+    
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+    document.body.style.backgroundColor=randomColor;
+    colorDisplay.textContent = `Background Color: ${randomColor}`;
+
+    
+
+}
